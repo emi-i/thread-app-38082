48fa2e08703dcfba6880de35125a3da76cf24515
emi-i <bacopatomato4@gmail.com>
2022-07-06 17:34:07 +0900

    rubocop

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :address do
-    post_code {"111-1111"}
-    address {"東京都"}
-    tel {"1234567890"}
-    emergency_contact_tel {"1234567890"}
-    emergency_contact_name {Faker::Name.name}
+    post_code { '111-1111' }
+    address { '東京都' }
+    tel { '1234567890' }
+    emergency_contact_tel { '1234567890' }
+    emergency_contact_name { Faker::Name.name }
   end
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  before do 
+  before do
     @address = FactoryBot.build(:address)
   end
 
