3b6d6f491ed972ae85815f945715fcb90471c3d0
emi-i <bacopatomato4@gmail.com>
2022-07-06 22:36:04 +0900

    diaty 投稿機能実装

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -3,8 +3,21 @@ class DiariesController < ApplicationController
   end
 
   def new
+    @diary = Diary.new
   end
 
   def create
+    @diary = Diary.new(diary_params)
+    if @diary.save
+      redirect_to diaries_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def diary_params
+    params.require(:diary).permit(:title, :date, :content).merge(user_id: current_user.id)
   end
 end
