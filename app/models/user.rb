7ba5f6980bd44e23ea663665c0565401a9846a7a
emi-i <bacopatomato4@gmail.com>
2022-07-06 16:24:09 +0900

    rubocopp

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, :nickname ,presence: true
+  validates :name, :nickname, presence: true
   has_one :address
 end
