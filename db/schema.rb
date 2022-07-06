367343034bc6cec6dcb6afc9c23e6a0791cbfeb1
emi-i <bacopatomato4@gmail.com>
2022-07-06 15:59:53 +0900

    addressモデル 追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_06_065302) do
+ActiveRecord::Schema.define(version: 2022_07_06_065833) do
+
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "post_code", null: false
+    t.text "address", null: false
+    t.string "tel", null: false
+    t.string "emergency_contact_tel", null: false
+    t.string "emergency_contact_name", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +38,5 @@ ActiveRecord::Schema.define(version: 2022_07_06_065302) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "addresses", "users"
 end
