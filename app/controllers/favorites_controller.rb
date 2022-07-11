536d11b710f721228d0133a94d40da053c4c5582
emi-i <bacopatomato4@gmail.com>
2022-07-11 14:51:19 +0900

    いいね　非同期化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,14 @@
 class FavoritesController < ApplicationController
 
     def create
+      @diary = Diary.find(params[:id])
       @favorite = Favorite.create(user_id: current_user.id, diary_id: params[:id])
-      redirect_to diary_path
     end
 
     def destroy
+      @diary = Diary.find(params[:id])
       @favorite = Favorite.find_by(user_id: current_user.id, diary_id: params[:id])
       @favorite.destroy
-      redirect_to diary_path
     end  
 
 end
