073df7557665b040c0476aa2fd03168df112f883
emi-i <bacopatomato4@gmail.com>
2022-07-14 17:39:34 +0900

    sns_credentialモデル　作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_12_060837) do
+ActiveRecord::Schema.define(version: 2022_07_14_083845) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "post_code", null: false
@@ -50,6 +50,15 @@ ActiveRecord::Schema.define(version: 2022_07_12_060837) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -68,4 +77,5 @@ ActiveRecord::Schema.define(version: 2022_07_12_060837) do
   add_foreign_key "diaries", "users"
   add_foreign_key "favorites", "diaries"
   add_foreign_key "favorites", "users"
+  add_foreign_key "sns_credentials", "users"
 end
